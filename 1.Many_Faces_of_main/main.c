6786ec08f80f0f41d1f2cb8f90fe78210967776a
ABHAI TIWARI <abhaytab444@gmail.com>
2026-01-06 12:10:13 +0000

    folder structure update

--- 1.Many_Faces_of_main/main.c
+++ 1.Many_Faces_of_main/main.c
@@ -3,6 +3,7 @@
 
 /*Usually Program Starts from main function*/
 /*main can be defined in multiple ways like : void main() or vaid main (void) or int main (vaid) ...lets see and understand how each one works differently*/
+/*Uncomment one main function at a time of compilation*/
 
 int main(void) {
     // Your code here
@@ -12,17 +13,17 @@ int main(void) {
 
 
 
-int main(int argc, char *argv[]) {
-    // Your code here
-    return 0;
-}
+// int main(int argc, char *argv[]) {
+//     // Your code here
+//     return 0;
+// }
 
 
-int main() {
-    // Your code here
-    return 0;
-}
+// int main() {
+//     // Your code here
+//     return 0;
+// }
 
-void main() {
-    // Code
-}
+// void main() {
+//     // Code
+// }
