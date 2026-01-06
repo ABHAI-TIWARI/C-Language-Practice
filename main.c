5b1dd7a4e8925cd22cf8cc40d9686ebcfa13cc8e
ABHAI TIWARI <abhaytab444@gmail.com>
2026-01-06 11:11:32 +0000

    first commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+/*Usually Program Starts from main function*/
+/*main can be defined in multiple ways like : void main() or vaid main (void) or int main (vaid) ...lets see and understand how each one works differently*/
+int main(void)
+{
+    printf("Welcome, ABHAI!\n");
+
+    return 0;
+}
